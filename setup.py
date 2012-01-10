5755488527905125540441b115f69cc908c53e9b
Souheil CHELFOUH <trollfot@gmail.com>
2012-01-10 18:48:35 +0100

    Added oc for release

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from os.path import join
 from setuptools import setup, find_packages
 
 name = 'dolmen.forms.crud'
-version = '2.0b1'
+version = '2.0b2'
 readme = open(join('src', 'dolmen', 'forms', 'crud', 'README.txt')).read()
 history = open(join('docs', 'HISTORY.txt')).read()
 
