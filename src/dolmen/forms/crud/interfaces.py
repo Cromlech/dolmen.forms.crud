433ede26b3d32e01dd79f322b07ac251c2271094
Souheil CHELFOUH <trollfot@gmail.com>
2018-01-09 00:32:48 +0100

    Removed duplicate of interfaces between cromlech.content and dolmen.forms.crud. Now, we really need to decide the future of cromlech.content or its simplification over time.

--- src/dolmen/forms/crud/interfaces.py
+++ src/dolmen/forms/crud/interfaces.py
@@ -1,26 +1,2 @@
-# -*- coding: utf-8 -*-
-
-from zope.schema import Object
-from zope.interface import Interface, Attribute
-from cromlech.content import IFactory
-
-
-class IAdding(Interface):
-    """Defines an abstraction layer for the adding mechanism.
-    Historically, the interface was located in the `zope.app.container`.
-    """
-    context = Attribute("The context of the adding object")
-    request = Attribute("The HTTP request")
-
-    def add(component):
-        """Adds the component in the context.
-        """
-
-
-class IFactoryAdding(IAdding):
-    """An IFactoryAdding extends an IAdding by adding the notion of Factory.
-    """
-    factory = Object(
-        missing_value=None,
-        title=u"The factory generating the content.",
-        schema=IFactory)
+# Exposed API
+from cromlech.content import IFactory, IAdding, IFactoryAdding
